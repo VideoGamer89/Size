0d7de28391e418a3c6ac4f430342fb59a7471006
Jeff Vogel <jefferyseanvogel89@gmail.com>
2018-08-07 13:03:18 -0500

    Create Size.cpp

---
+++ Size.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  long double apple;
+  
+  cout << "The size of an is integer is " << sizeof(int);
+  cout << " btyes.\n";
+  cout << "The size of a long integer is " << sizeof(long);
+  cout << " bytes.\n";
+  cout << "An apple can be eaten in " << sizeof(apple);
+  cout << " bytes!\n";
+  return 0;
+}
